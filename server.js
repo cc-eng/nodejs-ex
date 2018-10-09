ccf88502c279c3c67bb8d27a48d8a243a421ba71
clittle <clittle@device-solutions.com>
2018-10-09 19:54:35 -0400

    fix error

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
 app.get('/', function (req, res) {
   // try to initialize the db on every request if it's not already
