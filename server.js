c983b7fd2174499930f9bd814556c221c2c3e1c2
clittle <clittle@device-solutions.com>
2018-10-09 22:04:07 -0400

    fix when no body

--- server.js
+++ server.js
@@ -39,9 +39,12 @@ app.get('/vlinktest', function (req, res) {
 });
 
 app.post('/vlinktest', function (req, res) {
-    let num = req.body.slice(0,4);
     let foo = ''
-    let st = num.map(x => {foo += ' 0x' + x.toString(16)}).join(' ');
+    if(req.body.length > 5)
+    {
+      num = req.body.slice(0,4);
+      num.map(x => {foo += ' 0x' + x.toString(16)}).join(' ');
+    }
     dp('POST IP addr:' + req.ip + ' body length:' + req.body.length + ' data:' + foo);
     res.send('ACK');
 });
